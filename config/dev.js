c9dbeee82e55ff1d5e4ae502f1af0f8b8f8e6add
cambleck <bleckcam@gmail.com>
2021-05-23 11:16:07 -0500

    .gitignore

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:9Hm3VWaGhZkMtqxS@cluster0.hmxwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  redisUrl: "redis://127.0.0.1:6379",
-  accessKeyId: "AKIA4BFMFUEZUVP7WLVT",
-  secretAccessKey: "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",
-  cookieKey: "123123123",
-};
